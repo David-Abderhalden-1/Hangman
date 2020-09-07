d696a6bbc1101d40a828f62ca239837e210896e8
unknown <david.abderhalden@hotmail.com>
2020-09-07 16:53:04 +0200

    Big Changes to Topografie
    Added Class and Functions in Class. No more global variables.

--- Hangman.py
+++ Hangman.py
@@ -4,177 +4,224 @@ import random
 
 
 # Variables_1
-two_users = False
-single_player = False
 letter_index = 100
 play_again = True
 
-# Introduction
-print("\n\n" + '*' * 37 + " Hangman " + '*' * 37 + "\n")
-print("""\
-Description: You have to guess a word. As you guess wrong letters, you will get
-                          hanged time to time. Good luck!""")
-time.sleep(8)
-# Functions
 
-while play_again:
-    two_users = False
-    single_player = False
-    while not two_users and not single_player:
-        user_input = input("\n(a) You are two players (b) You are one player | Your answer: ")
-        if user_input.lower() == "a":
-            two_users = True
-        elif user_input.lower() == "b":
-            single_player = True
-        else:
-            time.sleep(2)
-            print("Error! The answer: " + user_input + " is not recognized.")
+# Functions&Classes
 
-    # Program
-    # Starter phase
-    # Variables_2
-    searched_word = ""
-    time.sleep(2)
-    word_entered = False
-    index_list = []
-    used_letters = []
-    maximum_tries = 6
-    user_tries = 0
-    found_letters = ""
-    phases = Phases()
-    i = 1
+class Functions:
+    #Variables
 
-    if two_users:
-        while not word_entered:
-            searched_word = input("\nPerson 1 enter your word here: ")
-            searched_word = searched_word.upper()
-            word = list(searched_word)
-            time.sleep(2)
-            user_answer = input("\nYour word is: " + searched_word +
-                                ". Do you want to proceed? (a) Yes (b) No | Your answer: ")
-            if user_answer.lower() == "a":
-                word_entered = True
-            elif user_answer.lower() == "b":
-                word_entered = False
+    # Introduction
+    def introduction(self):
+        print("\n\n" + '*' * 37 + " Hangman " + '*' * 37 + "\n")
+        print("""\
+    Description: You have to guess a word. As you guess wrong letters, you will get
+                                hanged time to time. Good luck!""")
+        time.sleep(8)
+
+
+    # Gamemode (Multiplayer or Singleplayer)
+    def gamemode(self):
+        self.two_users = False
+        self.single_player = False
+        user_input = ""
+        while not self.two_users and not self.single_player:
+            user_input = input("\n(a) You are two players (b) You are one player | Your answer: ")
+            if user_input.lower() == "a":
+                self.two_users = True
+            elif user_input.lower() == "b":
+                self.single_player = True
             else:
                 time.sleep(2)
-                print("Error! The answer: " + user_answer + " is not recognized.")
-            while i <= 20 and word_entered:
-                print("/" * 37 + 'barrier!' + "/" * 37)
-                i += 1
+                print("Error! The answer: " + user_input + " is not recognized.")
 
-    elif single_player:
-        single_player_words = open("words_1.0.txt", "r")
-        single_player_words = list(single_player_words)
-        index_count = -1
-        for indx in single_player_words:
-            index_count += 1
+        return self.two_users, self.single_player, user_input
 
-        searched_word = (single_player_words[random.randint(0, index_count)])
-        searched_word = searched_word.upper()
-        word = list(searched_word)
-        x = word.index("\n")
-        word = word[:x]
-        searched_word = searched_word[:x]
+    # Gamemode Setup (two or one user)
+    def gamemode_setup(self):
+        word_entered = False
+        self.searched_word = ""
+        self.word = ""
+        if self.two_users:
+            while not word_entered:
+                self.searched_word = input("\nPerson 1 enter your word here: ")
+                self.searched_word = self.searched_word.upper()
+                self.word = list(self.searched_word)
+                time.sleep(2)
+                user_answer = input("\nYour word is: " + self.searched_word +
+                                    ". Do you want to proceed? (a) Yes (b) No | Your answer: ")
+                if user_answer.lower() == "a":
+                    word_entered = True
+                elif user_answer.lower() == "b":
+                    word_entered = False
+                else:
+                    time.sleep(2)
+                    print("Error! The answer: " + user_answer + " is not recognized.")
+                i = 5
+                while i <= 20 and word_entered:
+                    print("/" * 37 + 'barrier!' + "/" * 37)
+                    i += 1
+
+        elif self.single_player:
+            single_player_words = open("words_1.0.txt", "r")
+            single_player_words = list(single_player_words)
+            index_count = -1
+            for indx in single_player_words:
+                index_count += 1
+
+            self.searched_word = (single_player_words[random.randint(0, index_count)])
+            self.searched_word = self.searched_word.upper()
+            self.word = list(self.searched_word)
+            x = self.word.index("\n")
+            self.word = self.word[:x]
+            self.searched_word = self.searched_word[:x]
+
+        return self.word, self.searched_word
 
     time.sleep(2)
 
-    # Game phase
-    for letter in word:
-        found_letters = found_letters + "-"
+    # create_found_letters
+    def create_found_letters(self):
+        self.found_letters = ""
+        for letter in self.word:
+            self.found_letters = self.found_letters + "-"
 
-    print("\nThe word is: " + found_letters)
+        print("\nThe word is: " + self.found_letters)
 
-    while found_letters != searched_word and not user_tries == maximum_tries:
+        return self.found_letters
 
-        user_letter = input("Enter a Letter: ")
-        user_letter = user_letter.upper()
-        Right_letter = False
+    # While not lost or won
+    def run_variables(self):
+        self.user_tries = 0
+        self.maximum_tries = 6
 
-        if word.count(user_letter) > 0:
-            i = word.count(user_letter)
+        return  self.found_letters, self.user_tries
+
+    # Input letter
+    def input_letter(self):
+        self.user_letter = input("Enter a Letter: ")
+        self.user_letter = self.user_letter.upper()
+        self.Right_letter = False
+
+    # check_float_letter
+    def check_float_letter(self):
+        self.index_list = []
+        self.used_letters = []
+        self.Right_letter = False
+        if self.word.count(self.user_letter) > 0:
+            i = self.word.count(self.user_letter)
             for index in range(i):
-                letter_index = (word.index(user_letter))
-                index_list.append(letter_index)
-                word[letter_index] = "*"
-                Right_letter = True
+                self.letter_index = (self.word.index(self.user_letter))
+                self.index_list.append(self.letter_index)
+                self.word[self.letter_index] = "*"
+                self.Right_letter = True
 
         else:
             time.sleep(2)
-            Right_letter = False
-            used_letters.append(user_letter)
+            self.Right_letter = False
+            self.used_letters.append(self.user_letter)
 
-        if Right_letter:
-            found_letters = ""
+            return self.Right_letter, self.index_list, self.used_letters, self.word
 
-            for letter in searched_word:
-                found_letters = found_letters + "-"
+    # process_right_or_wrong
+    def effect(self):
+        self.phases = Phases()
+        if self.Right_letter:
+            self.found_letters = ""
 
-            temp = list(found_letters)
+            for letter in self.searched_word:
+                self.found_letters = self.found_letters + "-"
 
-            for index in index_list:
-                letters = searched_word[index]
+            temp = list(self.found_letters)
+
+            for index in self.index_list:
+                letters = self.searched_word[index]
                 temp[index] = letters
             result = ''.join(temp)
-            found_letters = str(result)
+            self.found_letters = str(result)
 
         else:
-            user_tries += 1
-            print("The Letter: " + user_letter + " is not in the word!\n")
+            self.user_tries += 1
+            print("The Letter: " + self.user_letter + " is not in the word!\n")
             time.sleep(2)
-            if user_tries == 1:
-                phases.phase_1()
-            elif user_tries == 2:
-                phases.phase_2()
-            elif user_tries == 3:
-                phases.phase_3()
-            elif user_tries == 4:
-                phases.phase_4()
-            elif user_tries == 5:
-                phases.phase_5()
-            elif user_tries == 6:
-                phases.phase_6()
+            if self.user_tries == 1:
+                self.phases.phase_1()
+            elif self.user_tries == 2:
+                self.phases.phase_2()
+            elif self.user_tries == 3:
+                self.phases.phase_3()
+            elif self.user_tries == 4:
+                self.phases.phase_4()
+            elif self.user_tries == 5:
+                self.phases.phase_5()
+            elif self.user_tries == 6:
+                self.phases.phase_6()
 
+        return self.index_list, self.found_letters
+
+    # Output_Text (Presentation usw.)
+    def output(self):
         time.sleep(2)
-        print("\nThe word: " + found_letters + "\n")
+        print("\nThe word: " + self.found_letters + "\n")
         print("Wrong letters: ")
-        for letter in used_letters:
+        for letter in self.used_letters:
             print(letter + ", ", end="")
         print("\n")
         print("_" * 82)
+    # Check won or lost
+    # Ask play_again
 
-    if found_letters == searched_word:
-        print("\n\n" + "*" * 29 + ' $You Won$ ' + "*" * 29 + "\n")
 
-    elif user_tries == maximum_tries:
-        print("The word was: " + searched_word)
-        print("\n\n" + "*" * 29 + ' You Lost! Game Over ' + "*" * 29 + "\n")
+# main________________________________________________________________________________________________
 
-    time.sleep(4)
-    x = True
-    while x:
-        play_input = input("Do you want to play again? (a) Yes of course! (b) No maybe later. | Your answer: ")
-        if play_input.lower() == "a":
-            print("_" * 82)
-            play_again = True
-            x = False
-            time.sleep(2)
-        elif play_input.lower() == "b":
-            play_again = False
-            x = False
-            time.sleep(2)
-        else:
-            time.sleep(2)
-            print("The syntax: " + play_input + " is not recognized!\n")
-
-# Functions
 # Introduction
-# Gamemode (Multiplayer or Singleplayer)
-# Gamemode Setup (two or one user)
-# create_found_letters
-# Input letter
-# check_float_letter
-# process_right_or_wrong
-# Output_Text (Presentation usw.)
-# Check won or lost
-# Ask play_again
+f = Functions()
+f.introduction()
+
+# Gamemode
+while play_again:
+    f.gamemode()
+    f.gamemode_setup()
+    f.create_found_letters()
+    f.run_variables()
+    while f.found_letters != f.searched_word and not f.user_tries == f.maximum_tries:
+        f.input_letter()
+        f.check_float_letter()
+        f.effect()
+
+# Not yet done___________________________________________________________________________________________
+
+
+    time.sleep(2)
+    print("\nThe word: " + found_letters + "\n")
+    print("Wrong letters: ")
+    for letter in used_letters:
+        print(letter + ", ", end="")
+    print("\n")
+    print("_" * 82)
+
+if found_letters == searched_word:
+    print("\n\n" + "*" * 29 + ' $You Won$ ' + "*" * 29 + "\n")
+
+elif user_tries == maximum_tries:
+    print("The word was: " + searched_word)
+    print("\n\n" + "*" * 29 + ' You Lost! Game Over ' + "*" * 29 + "\n")
+
+time.sleep(4)
+x = True
+while x:
+    play_input = input("Do you want to play again? (a) Yes of course! (b) No maybe later. | Your answer: ")
+    if play_input.lower() == "a":
+        print("_" * 82)
+        play_again = True
+        x = False
+        time.sleep(2)
+    elif play_input.lower() == "b":
+        play_again = False
+        x = False
+        time.sleep(2)
+    else:
+        time.sleep(2)
+        print("The syntax: " + play_input + " is not recognized!\n")
